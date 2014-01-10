a277eebd91e31a1ae13d92cf84adc258f7d03d9a
Manuel Abrante <racerf5@gmail.com>
2014-01-10 11:43:27 -0430

    Buscar prestamo - prueba Query

--- JsonVenezuela90/buscarPrestamoLibro.php
+++ JsonVenezuela90/buscarPrestamoLibro.php
@@ -2,42 +2,44 @@
 // conexion bd 
 include("conex.php");
 //formato JSON
-header('Content-type: application/json');
+//header('Content-type: application/json');
 
-$val0= $_GET["val0"];
+//$val0= $_GET["val0"];
 $val1= $_GET["val1"];
 //$val2= $_GET["val2"];
-
+/*
 $sql1= "SELECT id_e_prestamo,nombre,cedula,grado,id_libro FROM estudiante_prestamo WHERE $val0 LIKE '%$val1%'";
 $result1 = mysql_query($sql1) or die("Error de Consulta". mysql_error());
-
+*/
+$sql1 = "Select P.id_prestamoLibro,U.Cedula,U.Nombre,U.Apellido,P.cedulaEstudiante,P.id_Libro,P.fecha_entrega,P.fecha_salida,L.id_libro,L.nombre_libro,L.autor_libro,L.materia_libro,L.year_libro,L.edit_libro,L.edicion_libro,L.cantidad_libro FROM  usuario U INNER JOIN prestamo_libro  P ON U.cedula = P.cedulaEstudiante INNER JOIN libros L ON P.id_Libro = L.id_libro WHERE U.Cedula = $val1";
+$result1 = mysql_query($sql1) or die("Error de Consulta". mysql_error());
 $e =0;
 $i=0;
 $cantidad = mysql_num_rows($result1);
 //$estado["num"]= $cantidad;
 while($row = mysql_fetch_row($result1)){
 	
-	$estado[$e]["idPres"]= $row[0];
-	$estado[$e]["nomPres"]= $row[1];
-	$estado[$e]["cedPres"]= $row[2];
-	$estado[$e]["graPres"]= $row[3];
-	$estado[$e]["idLibroPRes"]= $row[4];
+	$estado[$i]["idPres"]= $row[0];
+	$estado[$i]["nomPres"]= $row[2]." ".$row[3];
+	$estado[$i]["cedPres"]= $row[1];
+	$estado[$i]["graPres"]= $row[4];
+	$estado[$i]["idLibroPRes"]= $row[5];
 	
 	$sql2 = "SELECT id_libro,nombre_libro,autor_libro,materia_libro,year_libro,edit_libro,edicion_libro,cantidad_libro FROM libros WHERE id_libro= '$row[4]'";
+
 	$result2 = mysql_query($sql2) or die("Error de Consulta". mysql_error());
 
-while($row2 = mysql_fetch_row($result2)){
-	$estado[$i]["idLibro"]= $row2[0];
-	$estado[$i]["nom"]= $row2[1];
-	$estado[$i]["aut"]= $row2[2];
-	$estado[$i]["mat"]= $row2[3];
-	$estado[$i]["year"]= $row2[4];
-	$estado[$i]["edit"]= $row2[5];
-	$estado[$i]["edic"]= $row2[6];
-	$estado[$i]["cant"]= $row2[7];
-	$e++;
+	$cantidad2 = mysql_num_rows($result2);
+	
+	$estado[$i]["idLibro"]= $row[8];
+	$estado[$i]["nom"]= $row[9];
+	$estado[$i]["aut"]= $row[10];
+	$estado[$i]["mat"]= $row[11];
+	$estado[$i]["year"]= $row[12];
+	$estado[$i]["edit"]= $row[13];
+	$estado[$i]["edic"]= $row[14];
+	$estado[$i]["cant"]= $row[15];
 	$i++;
-}
 
 }
 if($cantidad == 0){
