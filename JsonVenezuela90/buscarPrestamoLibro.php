f533e6acef40f5b818ec0feb541e0372874ce521
Manuel Abrante <racerf5@gmail.com>
2014-01-25 10:19:01 -0430

    Prestamo Libros 100%

--- JsonVenezuela90/buscarPrestamoLibro.php
+++ JsonVenezuela90/buscarPrestamoLibro.php
@@ -6,7 +6,6 @@ include("conex.php");
 
 $val0= $_GET["val0"];
 $val1= $_GET["val1"];
-
 if($val0 == "cedula"){
 	$sql1 = "Select P.id_prestamoLibro,U.Cedula,U.Nombre,U.Apellido,P.cedulaEstudiante,P.id_Libro,P.fecha_entrega,P.fecha_salida,L.id_libro,L.nombre_libro,L.autor_libro,L.materia_libro,L.year_libro,L.edit_libro,L.edicion_libro,L.cantidad_libro FROM  usuario U INNER JOIN prestamo_libro  P ON U.cedula = P.cedulaEstudiante INNER JOIN libros L ON P.id_Libro = L.id_libro WHERE U.Cedula = $val1";
 }
@@ -14,6 +13,7 @@ else
 if($val0 == "dateP"){
 	$sql1 = "Select P.id_prestamoLibro,U.Cedula,U.Nombre,U.Apellido,P.cedulaEstudiante,P.id_Libro,P.fecha_entrega,P.fecha_salida,L.id_libro,L.nombre_libro,L.autor_libro,L.materia_libro,L.year_libro,L.edit_libro,L.edicion_libro,L.cantidad_libro FROM  usuario U INNER JOIN prestamo_libro  P ON U.cedula = P.cedulaEstudiante INNER JOIN libros L ON P.id_Libro = L.id_libro WHERE P.fecha_salida = '$val1'";
 }
+
 else
 if($val0 == "todo"){
 	$sql1 = "Select P.id_prestamoLibro,U.Cedula,U.Nombre,U.Apellido,P.cedulaEstudiante,P.id_Libro,P.fecha_entrega,P.fecha_salida,L.id_libro,L.nombre_libro,L.autor_libro,L.materia_libro,L.year_libro,L.edit_libro,L.edicion_libro,L.cantidad_libro FROM  usuario U INNER JOIN prestamo_libro  P ON U.cedula = P.cedulaEstudiante INNER JOIN libros L ON P.id_Libro = L.id_libro";
@@ -33,7 +33,7 @@ while($row = mysql_fetch_row($result1)){
 	$estado[$i]["cedPres"]= $row[1];
 	$estado[$i]["graPres"]= $row[4];
 	$estado[$i]["idLibroPRes"]= $row[5];
-	$algo = $row[5];
+	//$algo = $row[5];
 	
 	//$sql2 = "SELECT id_libro,nombre_libro,autor_libro,materia_libro,year_libro,edit_libro,edicion_libro,cantidad_libro FROM libros WHERE id_libro= '$algo'";
 
