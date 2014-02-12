15fa5d8e7b87f7864c17460ee25aec7402063885
CindyS-5 <cindysalinas15@gmail.com>
2014-02-11 19:31:06 -0430

    Modificaciones Diseño Carga De Notas

--- js/cargaCalificacionesDocente.js
+++ js/cargaCalificacionesDocente.js
@@ -1,5 +1,10 @@
 $(document).on("ready", empezar);
 var idDocente;
+
+var cedulaIngresarPA, materiaIngresarPA, yearIngresarPA, lapsoIngresarPA;
+
+var yearIngresarPG, materiaIngresarPG, yearIngresarPG, lapsoIngresarPG;
+
 function empezar(){
 	var cedula2=$.cookie("cedulaProf");
 
@@ -18,7 +23,29 @@ function empezar(){
 			});			
 		});
 	});
-	/*$('.linkAtrasTablaIngresarPEL1').on("click",function(){actionBotones('formIngresarPEL1','divIngresarPEL1');});*/
+	$('.ingresarMP').on("click",function(){actionBotones('ingresarMenu','menuPrincipal');});
+	$('.consultarMP').on("click",function(){actionBotones('consultarMenu','menuPrincipal');});
+	$('.modificarMP').on("click",function(){actionBotones('modificarMenu','menuPrincipal');});
+
+	$('.linkAtrasIngresar1').on("click",function(){actionBotones('menuPrincipal','ingresarMenu');});
+	$('.linkAtrasConsultar1').on("click",function(){actionBotones('menuPrincipal','consultarMenu');});
+	$('.linkAtrasModificar1').on("click",function(){actionBotones('menuPrincipal','modificarMenu');});
+
+	$('.ingresarMPP').on("click",function(){actionBotones('ingresarAlumno','ingresarMenu');});
+	$('.ingresarMPPG').on("click",function(){actionBotones('ingresarGrado','ingresarMenu');});
+
+	$('.linkAtrasIngresarPG1').on("click",function(){actionBotones('ingresarMenu','ingresarGrado');});
+	$('.linkAtrasIngresarPA1').on("click",function(){actionBotones('ingresarMenu','ingresarAlumno');});
+	$('.linkAtrasIngresarPA2').on("click",function(){actionBotones('divIngresarPA1','tablaIngresarPA1');});
+	$('.linkAtrasIngresarPA3').on("click",function(){actionBotones('tablaIngresarPA1','divIngresarPA2');});
+	$('.linkAtrasIngresarPG1').on("click",function(){actionBotones('ingresarMenu','ingresarGrado');});
+	$('.linkAtrasIngresarPG2').on("click",function(){actionBotones('divIngresarPG1','tablaIngresarPG1');});
+
+	$('#aceptarCedulaIngresarPA').on("click",ingresarCedulaAlumnoPA);
+	$('#consultarDatosIngresaPA').on("click",consultarIngresarDatosAlumnoPA);
+	$('#guardarIngresarPA').on("click",ingresarNotasPA);
+	$('#aceptarGradoIngresarPG').on("click",consultarIngresarDatosYearPG);
+	$('#consultarDatosIngresaPG').on("click",consultarIngresarDatosPG);
 }
 
 function resetear()
@@ -33,4 +60,40 @@ function actionBotones(mostrar,ocultar)
 	resetear();
 	$('.'+mostrar).show("slide");
 	$('.'+ocultar).hide("slow");	
+}
+
+function ingresarCedulaAlumnoPA() 
+{
+	cedulaIngresarPA=$("#inputCedulaIngresarPA").val();
+	actionBotones('tablaIngresarPA1','divIngresarPA1');	
+}
+
+function consultarIngresarDatosAlumnoPA() 
+{
+	materiaIngresarPA=$("#selectMateriaIngresarPA option:selected").val();
+	yearIngresarPA=$("#selectYearIngresarPA option:selected").val();
+	lapsoIngresarPA=$("#selectLapsoIngresarPA option:selected").val();
+
+	actionBotones('divIngresarPA2','tablaIngresarPA1');	
+}
+function ingresarNotasPA()
+{
+	var descripcion = $("#descrip").val();
+	var nota = $("#nota").val();
+	var porcen = $("#porc").val();
+}
+
+function consultarIngresarDatosYearPG() 
+{
+	yearIngresarPG=$("#gradoIngresarPG option:selected").val();
+	actionBotones("tablaIngresarPG1","divIngresarPG1");
+}
+
+function consultarIngresarDatosPG()
+{
+	materiaIngresarPG=$("#selectMateriaIngresarPG option:selected").val();
+	yearIngresarPG=$("#selectYearIngresarPG option:selected").val();
+	lapsoIngresarPG=$("#selectLapsoIngresarPG option:selected").val();
+	alert(materiaIngresarPG+yearIngresarPG+lapsoIngresarPG);
+	actionBotones("divIngresarPG2","tablaIngresarPG1");
 }
