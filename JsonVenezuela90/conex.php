afc866595263bfa2c939119d0a34ff669b6c1831
Manuel Abrante <racerf5@gmail.com>
2014-04-19 13:28:53 -0430

    ml

--- JsonVenezuela90/conex.php
+++ JsonVenezuela90/conex.php
@@ -1,9 +1,9 @@
 <?php
 
 //Conexión a base de datos
-$server = "127.0.0.1";
-$username = "root";
-$password = "";
+$server = "sql108.byetcluster.com";
+$username = "n260m_14382178";
+$password = "venezuel";
 $database = "venezuela90";
 $con = mysql_connect($server, $username, $password) or die ("Error al conectar: " . mysql_error());
 mysql_query("SET NAMES 'utf8'");
